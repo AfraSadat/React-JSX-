c5338071c14906114586a21b1011633449bd599f
Afra Sadat <110224244+AfraSadat@users.noreply.github.com>
2023-03-26 00:11:17 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello';
+import { Component } from 'react';
+class App extends Component{
+  render() {
+    return(
+      <div className="App">
+        <Hello/>
+      </div>
+    );
+  }
+}
+
+export default App;
